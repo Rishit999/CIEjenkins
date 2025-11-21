16feae210b8d8ad978eee58b8a2fada895be959d
Rishit999 <rishitnayakar@gmail.com>
2025-11-21 10:34:50 +0530

    Update pythoncal.py

--- pythoncal.py
+++ pythoncal.py
@@ -1,13 +1,11 @@
 import sys
 if (sys.argv)!= 3:
     print("Usage: python pythoncal.py <Integer1> <Integer2>")
+
+else:
     integer1 = int(sys.argv[1])
     integer2 = int(sys.argv[2])
     print(f"Integer1 {integer1} and Integer2 {integer2}")
-else:
-    
-    integer1 = 20
-    integer2 = 2
 
     if integer1 > 0:
         print(f"{integer1} is a positive number")
@@ -22,4 +20,3 @@ else:
         print(f"{integer2} is negative number")
     else:
         print(f"{integer2} is neither negative nor positive")
-   
